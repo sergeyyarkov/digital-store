87d511215458e3338b6ef3a6f60809fd76c9deb8
Sergey Yarkov <serzh.yarkov@gmail.com>
2019-11-11 19:05:24 +0300

    webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['@babel/polyfill', './src/js/index.js'],
   output: {
-    path: `${__dirname}/dist`,
+    path: `${__dirname}/dist/public/js`,
     filename: 'bundle.js',
   },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  }
 };
