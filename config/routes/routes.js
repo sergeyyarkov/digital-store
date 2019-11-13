e4585f88838f51e31e6452a650bbcd4b2d8f482f
Sergey Yarkov <serzh.yarkov@gmail.com>
2019-11-13 18:26:38 +0300

    Added all routes

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -1,5 +1,34 @@
-module.exports = function(app) {
-    app.get('/', (req, res) => {
+const routes = {
+    // Index
+    indexRoute: function(req, res) {
         res.render('index');
-    });
+    }, 
+
+    // How-to-buy
+    howToBuyRoute: function(req, res) {
+        res.render('how-to-buy');
+    },
+
+    // Contacts
+    contactsRoute: function(req, res) {
+        res.render('contacts');
+    },
+
+    // Comments
+    commentsRoute: function(req, res) {
+        res.render('comments');
+    },
+    
+    // My-orders
+    myOrdersRoute: function(req, res) {
+        res.render('my-orders');
+    }
+}
+
+module.exports = function(server) {
+    server.get('/', routes.indexRoute);
+    server.get('/how-to-buy', routes.howToBuyRoute);
+    server.get('/contacts', routes.contactsRoute);
+    server.get('/comments', routes.commentsRoute);
+    server.get('/my-orders', routes.myOrdersRoute)
 }
--- config/server.js
+++ config/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const app = express();
+const server = express();
 const path = require('path');
 
 // Pug config
-app.use(express.static(path.join(__dirname, '../dist/public')));
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '../dist/views'));
+server.use(express.static(path.join(__dirname, '../dist/public')));
+server.set('view engine', 'pug');
+server.set('views', path.join(__dirname, '../dist/views'));
 
-
-require('./routes/routes')(app);
-
+require('./routes/routes')(server);
 
 // 404 Redirect
-app.use((req, res) => res.status(404).send('Error 404'));
+server.use((req, res) => res.status(404).send('Error 404'));
 
-app.listen(3000, () => console.log('Listen port 3000...'));
+server.listen(3000, () => console.log('Listen port 3000...'));
