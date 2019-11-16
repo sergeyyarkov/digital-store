3f13519744298c3e2495ea1deb0cb6a844671ea9
Sergey Yarkov <serzh.yarkov@gmail.com>
2019-11-16 17:18:29 +0300

    Get Items api

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -1,3 +1,4 @@
+const MongoClient = require('mongodb').MongoClient;
 const routes = {
     // Index
     indexRoute: function(req, res) {
@@ -32,6 +33,36 @@ const routes = {
         res.render('my-orders', {
             pageName: 'my-orders'
         });
+    },
+
+    // GetItems
+    getItemsRoute: function(req, res) {
+        const queryOptions = {
+            auth: 'yYNfW8ynVO18L1TW5qIkILM1WtWgrVZz',
+            option: 'all'
+        }
+
+        if (req.query.auth === queryOptions.auth) {
+            if (req.query.option === queryOptions.option) {
+                MongoClient.connect('mongodb://localhost:27017/digital_store', {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                }, (err, db) => {
+                    if (err) throw err;
+
+                    const dbo = db.db('digital_store');
+                    dbo.collection('storeInfo').find().toArray((err, result) => {
+                        if (err) throw err;
+                        res.send(result);
+                        db.close();
+                    })
+                });
+            } else {
+                res.send('Invalid');
+            }
+        } else {
+            res.send('Invalid');
+        }
     }
 }
 
@@ -40,5 +71,6 @@ module.exports = function(server) {
     server.get('/how-to-buy', routes.howToBuyRoute);
     server.get('/contacts', routes.contactsRoute);
     server.get('/comments', routes.commentsRoute);
-    server.get('/my-orders', routes.myOrdersRoute)
+    server.get('/my-orders', routes.myOrdersRoute);
+    server.get('/store-api', routes.getItemsRoute);
 }
