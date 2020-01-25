e43ea0f34be6080d6e59fd55cb2d4ca3f08d8983
Sergey Yarkov <serzh.yarkov@gmail.com>
2020-01-25 22:16:37 +0300

    disqus comments

---
+++ dist/public/js/disqus.js
@@ -0,0 +1,6 @@
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://justatest-2.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
